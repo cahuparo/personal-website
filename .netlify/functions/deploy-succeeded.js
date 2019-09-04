84a05ab330ad5507aaee95d6d61b2c21bd73a99f
Julian Pawlowski <julian.pawlowski@gmail.com>
2019-09-04 16:46:27 +0200

    no message

--- .netlify/functions/deploy-succeeded.js
+++ .netlify/functions/deploy-succeeded.js
@@ -2,10 +2,11 @@ const request = require('request');
 
 exports.handler = function(event, context, callback) {
   var e = JSON.parse(event.body);
+  const { WEBSUB_HUB } = process.env;
   if (e.payload.context == "production") {
-    console.log(`[WebSub] preparing to ping ${WEBSUB_HUB}`)
+    console.log(`[WebSub] preparing to ping WEBSUB_HUB`)
     request.post(
-      `${WEBSUB_HUB}?hub.mode=publish&hub.url=${e.payload.url}/*`,
+      `WEBSUB_HUB?hub.mode=publish&hub.url=${e.payload.url}/*`,
       function (error, response, body){
         if (!error && response.statusCode == 204) {
           console.log("[WebSub] ping successful");
