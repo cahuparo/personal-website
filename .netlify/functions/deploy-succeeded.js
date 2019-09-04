7ecd831ae5629129c44577b07f895b0cbc72b6e2
Julian Pawlowski <julian.pawlowski@gmail.com>
2019-09-04 16:26:56 +0200

    no message

--- .netlify/functions/deploy-succeeded.js
+++ .netlify/functions/deploy-succeeded.js
@@ -3,15 +3,15 @@ const request = require('request');
 exports.handler = function(event, context, callback) {
   var e = JSON.parse(event.body);
   if (e.payload.context == "production") {
-    console.log(`[superfeedr] preparing to ping jpawlowski.superfeedr.com`)
+    console.log(`[WebSub] preparing to ping ${WEBSUB_HUB}`)
     request.post(
-      `https://jpawlowski.superfeedr.com/?hub.mode=publish&hub.url=${e.payload.url}/*`,
+      `${WEBSUB_HUB}?hub.mode=publish&hub.url=${e.payload.url}/*`,
       function (error, response, body){
         if (!error && response.statusCode == 204) {
-          console.log("[superfeedr] ping successful");
+          console.log("[WebSub] ping successful");
           callback(null, {statusCode: 204});
         } else {
-          console.log("[superfeedr] ping failed:", error);
+          console.log("[WebSub] ping failed:", error);
           callback(error, {statusCode: response.statusCode});
         }
       }
