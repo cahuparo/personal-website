e629adc3b4f97162447418da748e1a54bbfc7823
Julian Pawlowski <julian.pawlowski@gmail.com>
2019-09-04 17:24:14 +0200

    no message

--- .netlify/functions/deploy-succeeded.js
+++ .netlify/functions/deploy-succeeded.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
+const request = require('request')
+const { WEBSUB_HUB } = process.env
 exports.handler = function(event, context, callback) {
   var e = JSON.parse(event.body);
-  const { WEBSUB_HUB } = process.env;
   if (e.payload.context == "production") {
     console.log("[WebSub] preparing to ping " + WEBSUB_HUB)
-    const request = require('request')
     request.post(
-      `WEBSUB_HUB?hub.mode=publish&hub.url=${e.payload.url}/*`,
+      WEBSUB_HUB + "?hub.mode=publish&hub.url=" + e.payload.url + "/*",
       function (error, response, body){
         if (!error && response.statusCode == 204) {
           console.log("[WebSub] ping successful");
